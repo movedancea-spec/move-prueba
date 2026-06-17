dd493260e5dc8d8d45bc385ac0d9160b4818c509
movedancea-spec <movedancea@gmail.com>
2026-06-17 15:12:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function showImage(imageName) {
 
+    const img = document.getElementById("scheduleImage");
+
+    img.style.opacity = "0";
+
+    setTimeout(() => {
+
+        img.src = imageName;
+
+        img.onload = () => {
+            img.style.opacity = "1";
+        };
+
+    }, 250);
+}
+
+
+// Animación suave al cargar
+
+window.addEventListener("load", () => {
+
+    const img = document.getElementById("scheduleImage");
+
+    img.style.transition = "opacity .35s ease";
+
+});
